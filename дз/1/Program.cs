029e49aef5def6cd79b239cdefa890e03b21b065
Denya1710 <125318785+Denya1710@users.noreply.github.com>
2023-02-23 19:26:44 +0200

    Add files via upload

---
+++ дз/1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Запуск консоли...");
+Console.Write ("Введите A ");
+int a = int.Parse (Console.ReadLine ()!);
+
+Console.Write ("Введите B ");
+int b = int.Parse (Console.ReadLine ()!);
+int set = a;
+
+for (int i = 1; i < b; i ++) 
+  set = set*a;
+ Console.WriteLine($"Равно {set}");
